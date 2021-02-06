95df446f5709e9c0789d55f89362c6bdbd58758c
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-06 18:47:12 +0900

    add views and likes filed in plant

--- models/plant.js
+++ models/plant.js
@@ -12,8 +12,14 @@ module.exports = class Plant extends Model{
         return super.init({
             name: DataTypes.STRING,
             description: DataTypes.STRING,
-            views: DataTypes.INTEGER, //TODO: views와 likes는 default integer 0 으로 시작
-            likes: DataTypes.INTEGER // TODO: 추후 이 식물의 디테일 검색을 해서 들어올 경우 조회수 + 1
+            views: {
+                type: DataTypes.INTEGER,
+                defaultValue: 0,
+            }, //TODO: views와 likes는 default integer 0 으로 시작
+            likes: {
+                type: DataTypes.INTEGER,
+                defaultValue: 0
+            }
             // TODO: 좋아요 버튼을 누를 경우 좋아요 + 1
         }, {
             sequelize,
